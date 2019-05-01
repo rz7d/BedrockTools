ac875006faa25054ec39e5ea063024c70d627c77
azure <rz7@outlook.jp>
2019-05-01 14:45:01 +0900

    Version with git head hash

--- src/main/java/com/github/stilllogic20/bedrocktools/BedrockToolsMod.java
+++ src/main/java/com/github/stilllogic20/bedrocktools/BedrockToolsMod.java
@@ -6,12 +6,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 
-@Mod(modid = BedrockToolsMod.MODID, name = BedrockToolsMod.NAME, version = BedrockToolsMod.VERSION)
+@Mod(modid = BedrockToolsMod.MODID)
 public final class BedrockToolsMod {
 
     public static final String MODID = "bedrocktools";
-    public static final String NAME = "BedrockTools";
-    public static final String VERSION = "1.1-beta-";
 
     @SidedProxy(modId = MODID, clientSide = "com.github.stilllogic20.bedrocktools.client.ClientProxy", serverSide = "com.github.stilllogic20.bedrocktools.common.CommonProxy")
     public static CommonProxy proxy;
