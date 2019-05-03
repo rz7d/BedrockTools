cf83f970ef4701f7ac52c6b68825b265fc8d8411
azure <rz7@outlook.jp>
2019-05-03 12:09:58 +0900

    Add tests for VeinMode#next

--- src/test/java/com/github/stilllogic20/bedrocktools/common/item/ItemBedrockPickaxeTest.java
+++ src/test/java/com/github/stilllogic20/bedrocktools/common/item/ItemBedrockPickaxeTest.java
@@ -5,17 +5,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import com.github.stilllogic20.bedrocktools.common.item.ItemBedrockPickaxe.MiningMode;
+import com.github.stilllogic20.bedrocktools.common.item.ItemBedrockPickaxe.VeinMode;
 
 public class ItemBedrockPickaxeTest {
 
     @Test
-    public void testNext() {
+    public void testMiningModeNext() {
         assertEquals(MiningMode.MIDDLE, MiningMode.NORMAL.next());
         assertEquals(MiningMode.SLOW, MiningMode.MIDDLE.next());
         assertEquals(MiningMode.FAST, MiningMode.SLOW.next());
         assertEquals(MiningMode.INSANE, MiningMode.FAST.next());
         assertEquals(MiningMode.OFF, MiningMode.INSANE.next());
         assertEquals(MiningMode.NORMAL, MiningMode.OFF.next());
+    }
+
+    @Test
+    public void testVeinModeNext() {
+        assertEquals(VeinMode.MORE, VeinMode.NORMAL.next());
+        assertEquals(VeinMode.INSANE, VeinMode.MORE.next());
+        assertEquals(VeinMode.OFF, VeinMode.INSANE.next());
+        assertEquals(VeinMode.NORMAL, VeinMode.OFF.next());
     }
 
 }
