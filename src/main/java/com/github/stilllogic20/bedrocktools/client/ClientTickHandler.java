899d2a9fe751338683c255b01fb586ef3ca8b784
azure <rz7@outlook.jp>
2019-05-03 16:25:39 +0900

    Delete unused codes

--- src/main/java/com/github/stilllogic20/bedrocktools/client/ClientTickHandler.java
+++
@@ -1,9 +0,0 @@
-package com.github.stilllogic20.bedrocktools.client;
-
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-@SideOnly(Side.CLIENT)
-public class ClientTickHandler {
-
-}
--- src/main/java/com/github/stilllogic20/bedrocktools/common/item/ItemEnergeticBedrock.java
+++
@@ -1,5 +0,0 @@
-package com.github.stilllogic20.bedrocktools.common.item;
-
-public class ItemEnergeticBedrock {
-
-}
