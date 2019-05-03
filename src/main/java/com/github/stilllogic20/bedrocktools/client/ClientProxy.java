b83ceec5b43ee9adb2b888107fa3d35ed2f6d7fb
azure <rz7@outlook.jp>
2019-05-03 15:24:38 +0900

    Unified initializing method

--- src/main/java/com/github/stilllogic20/bedrocktools/client/ClientProxy.java
+++ src/main/java/com/github/stilllogic20/bedrocktools/client/ClientProxy.java
@@ -5,21 +5,16 @@ import com.github.stilllogic20.bedrocktools.client.init.KeyBindings;
 import com.github.stilllogic20.bedrocktools.client.init.Renders;
 import com.github.stilllogic20.bedrocktools.common.CommonProxy;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-
-import java.util.stream.Stream;
 
 @SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
     @Override
     public void construct(FMLConstructionEvent event) {
         super.construct(event);
-        Stream
-          .of(new Renders(),new ClientTickHandler())
-          .forEach(MinecraftForge.EVENT_BUS::register);
+        Renders.init();
         CreativeTabs.init();
         KeyBindings.init();
     }
--- src/main/java/com/github/stilllogic20/bedrocktools/client/init/Renders.java
+++ src/main/java/com/github/stilllogic20/bedrocktools/client/init/Renders.java
@@ -8,12 +8,17 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public final class Renders {
+
+    public static void init() {
+        MinecraftForge.EVENT_BUS.register(new Renders());
+    }
 
     @SubscribeEvent
     public void registerModels(ModelRegistryEvent event) {
